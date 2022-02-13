bbf00d066fc293c9efcc4759f2492c88c36ef472
Jacob Cornwell <jrcornwell96@gmil.com>
2022-02-13 17:00:03 +0000

    removes white space

--- script.js
+++ script.js
@@ -51,4 +51,3 @@ document.querySelector('.again').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem'
 });
-
