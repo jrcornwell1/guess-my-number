11b2d96f63b3463ba5abfeae057571fedbd68668
Jacob Cornwell <jrcornwell96@gmil.com>
2022-02-13 17:07:03 +0000

    adds displayMessage function for refactoring

--- script.js
+++ script.js
@@ -7,15 +7,20 @@ let highscore = 0;
 
 // document.querySelector('.number').textContent = secretNumber;
 
+const displayMessage = function (message) {
+    return document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
     // Changes guess to number
     const guess = Number(document.querySelector('.guess').value);
 
+    // When player doesn't guess a number
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number!';
+        displayMessage('No number!');
         // When player wins
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number!';
+        displayMessage('Correct Number!')
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
@@ -29,11 +34,11 @@ document.querySelector('.check').addEventListener('click', function () {
     } else if (guess !== secretNumber) {
         if (score > 1) {
             // Uses ternary op to return a value
-            document.querySelector('.message').textContent = guess > secretNumber ? 'Too high!' : 'Too low!';
+            displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
+            displayMessage('You lost the game!');
             document.querySelector('.score').textContent = 0;
         }
     }
