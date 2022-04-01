04692ad7efd22b90e49d4696afebe3af8e0623fc
Jacob Cornwell <jrcornwell96@gmil.com>
2022-04-01 15:08:40 +0100

    makes comments Todo:

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+// Function to reduct document.querySelector call
 const displayMessage = function (message) {
     return document.querySelector('.message').textContent = message;
 }
 
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     // Changes guess to number
     const guess = Number(document.querySelector('.guess').value);
+
+    // add function for guess === same guess return string 'Same guess'
 
     // When player doesn't guess a number
     if (!guess) {
@@ -21,11 +24,13 @@ document.querySelector('.check').addEventListener('click', function () {
         // When player wins
     } else if (guess === secretNumber) {
         displayMessage('Correct Number!')
+
+        // refactor here to use function - make DRY 
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
 
-        // Sets highscore value
+        // Sets highscore value - nested if 
         if (score > highscore) {
             highscore = score;
             document.querySelector('.highscore').textContent = highscore;
